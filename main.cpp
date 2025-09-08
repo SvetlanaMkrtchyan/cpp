76ff820695d596c224ef90099925766c822f15f8
SvetlanaMkrtchyan <lanmkrtchyanbarca@gmail>
2025-09-08 15:05:30 +0400

    Homework 1 (xor)

--- main.cpp
+++ main.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 
-int main(){
-    int n=7;
-    int x[7]={0, 1, 3, 7, 7, 0, 1}; 
-    for (int i=0; i<n; i++){
-        int count=0;
-        for (int j=0; j<n; j++){
-            if(x[i]==x[j]){
-                count++;
-            }
-        }
-        if (count==1) {
-            std::cout<<x[i];
-            break;
-        }
+int main() {
+    int n = 7;
+    int x[7] = {0, 1, 3, 7, 7, 0, 1};
+    int temp=0;
+    for (int i = 0; i < n; i++) {
+        temp ^= x[i];
     }
+    std::cout<<temp;
     return 0;
 }
