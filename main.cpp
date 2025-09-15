b54c46606a54fbf6ad50d08785720fa5d28de8b0
SvetlanaMkrtchyan <lanmkrtchyanbarca@gmail>
2025-09-15 14:06:34 +0400

    Homework 2

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 
 int main() {
-    int n = 7;
-    int x[7] = {0, 1, 3, 7, 7, 0, 1};
-    int temp=0;
-    for (int i = 0; i < n; i++) {
-        temp ^= x[i];
+    int n=10;
+    int x[10]={1, 3, 5, 2, 3, 2, 1, 6, 6, 4};
+    std::cout<<"Unique elements:";
+    for(int i=0; i<n; ++i){
+        bool found=false;
+        for(int j=0; j<n; ++j)
+            if(i!=j && x[i]==x[j]){
+                found=true;
+                break;
+            }
+        if(!found)
+            std::cout<<x[i]<<" ";
     }
-    std::cout<<temp;
-    return 0;
 }
